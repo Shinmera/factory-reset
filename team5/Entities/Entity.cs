2b23fbd1bd67be6e040270d8b2fd4be673a987f5
2weirdy <lig@student.ethz.ch>
2019-03-16 20:57:56 +0100

    Added Entity skeleton
    Added Level skeleton

---
+++ team5/Entities/Entity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace team5
+{
+	class Entity
+	{
+
+		Rectangle boundingBox;
+		Vector2 velocity;
+		Vector2 position;
+
+		public virtual void Update(GameTime gameTime)
+		{
+
+		}
+
+		public virtual void Draw(GameTime gameTime)
+		{
+
+		}
+
+		//Assume source is always a box
+		public virtual bool collide(Entity source, ref int direction, ref Vector2 position)
+		{
+			return false;
+		}
+	}
+}
---
+++ team5/Level.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace team5
+{
+	class Level
+	{
+		public static int EMPTY = 0;
+		public static int SOLIDPLATFORM = 1;
+
+		public int[][] tileset;
+
+		//Viewcones, intelligence
+		ArrayList CollidingEntities;
+
+		//Enemies, background objects
+		ArrayList NonCollidingEntities;
+
+		//things that will stop you like moving platforms (which are not part of the tileset)
+		ArrayList SolidEntities;
+
+
+
+		public Level()
+		{
+			ArrayList SolidEntities = new ArrayList();
+		}
+
+		public void Update(GameTime gameTime)
+		{
+			foreach (var entity in SolidEntities)
+			{
+				((Entity)entity).Update(gameTime);
+			}
+		}
+
+		public void Draw(GameTime gameTime)
+		{
+
+		}
+
+		public bool collide(Entity source, ref int direction, ref Entity target, ref Vector2 position)
+		{
+			return false;
+		}
+	}
+}
