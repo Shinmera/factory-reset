6286fa08d4a8b397dccefc6957aef5bf4ae8c894
Amirhossein Heidari <amir74heidari@gmail.com>
2019-03-19 12:35:04 +0100

    basic animation class

---
+++ team5/Components/AnimatedSprite.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+
+
+namespace team5
+{   
+    //Basic animation class for sprites
+    public class AnimatedSprite
+    {
+        public Texture2D Texture { get; set; }
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        private int currentFrame;
+        private int totalFrames;
+
+
+        public AnimatedSprite(Texture2D texture, int rows, int columns)
+        {
+            Texture = texture;
+            Rows = rows;
+            Columns = columns;
+            currentFrame = 0;
+            totalFrames = Rows * Columns;
+        }
+
+        public void Update()
+        {
+            currentFrame++;
+            if (currentFrame == totalFrames)
+                currentFrame = 0;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+            int width = Texture.Width / Columns;
+            int hight = Texture.Height / Rows;
+            int row = (int)((float)currentFrame / (float)Columns);
+            int column = currentFrame % Columns;
+
+            Rectangle sourceRectangle = new Rectangle(width * column, hight * row, width, hight);
+            Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, width, hight);
+            spriteBatch.Begin();
+            spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
+            spriteBatch.End();
+
+        }
+    }
+}
