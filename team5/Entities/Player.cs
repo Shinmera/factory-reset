2bc867ac9e0c5958c8677536ecf1ef7774a4a854
Shinmera <shinmera@tymoon.eu>
2019-03-26 14:17:14 +0100

    Much more satisfying movement

--- team5/Entities/Player.cs
+++ team5/Entities/Player.cs
@@ -23,12 +23,14 @@ namespace team5
         private float LongJump = 0;
 
         private float Gravity = 800;
-        private float MaxVel = 200;
-        private float AccelRate = 600;
+        private float MaxVel = 150;
+        private float AccelRate = 800;
+        private float DeaccelRate = 100;
         private float ClimbSpeed = 70;
         private float JumpSpeed = 150;
         private float LongJumpSpeed = 250;
         private float LongJumpTime = 15;
+        private Vector2 WallJumpVelocity = new Vector2(100, -200);
         private float WallSlideFriction = 0.9F;
 
         public Player(Vector2 position, Game1 game):base(game, new Vector2(Chunk.TileSize, Chunk.TileSize))
@@ -65,6 +67,9 @@ namespace team5
             Object right= chunk.CollidePoint(new Vector2(Position.X+Size.X+1,
                                                          Position.Y+Size.Y/2));
             
+            // Apply gravity
+            Velocity.Y += dt * Gravity;
+            
             IsClimbing = false;
             if (down != null)
             {
@@ -73,7 +78,7 @@ namespace team5
                 if (Jump)
                 {
                     Jump = false;
-                    Velocity.Y -= JumpSpeed;
+                    Velocity.Y = -JumpSpeed;
                     LongJump = LongJumpTime*dt;
                 }
             }
@@ -83,47 +88,56 @@ namespace team5
                 if(Controller.Climb)
                 {
                     IsClimbing = true;
-                    if(Controller.MoveUp)
+                    if(Controller.MoveUp && -ClimbSpeed < Velocity.Y)
                         Velocity.Y = -ClimbSpeed;
                     else if(Controller.MoveDown)
                         Velocity.Y = +ClimbSpeed;
-                    else
+                    else if(-ClimbSpeed <= Velocity.Y)
                         Velocity.Y = 0;
                 }
-                else if(Velocity.Y > 0)
+                else if(0 < Velocity.Y)
                     Velocity.Y *= WallSlideFriction;
 
-                if (Jump && (!HasWallJumped || CanRepeatWallJump) && right != null)
+                if (Jump && (!HasWallJumped || CanRepeatWallJump))
                 {
-                    Velocity.Y -= JumpSpeed;
-                    Velocity.X = -MaxVel;
-                    HasWallJumped = true;
-                    Jump = false;
-                }
-                if (Jump && (!HasWallJumped || CanRepeatWallJump) && left != null)
-                {
-                    Velocity.Y -= JumpSpeed;
-                    Velocity.X = MaxVel;
-                    HasWallJumped = true;
-                    Jump = false;
+                    if (right != null)
+                    {
+                        Velocity.X = (Controller.MoveRight)? 0 : -WallJumpVelocity.X;
+                        Velocity.Y = WallJumpVelocity.Y;
+                        HasWallJumped = true;
+                        Jump = false;
+                    }
+                    else if (left != null)
+                    {
+                        Velocity.X = (Controller.MoveLeft)? 0 : WallJumpVelocity.X;
+                        Velocity.Y = WallJumpVelocity.Y;
+                        HasWallJumped = true;
+                        Jump = false;
+                    }
                 }
             }
             
-            if(!IsClimbing){
+            if(!IsClimbing || down != null){
                 if(Controller.MoveRight && Velocity.X < MaxVel)
                 {
+                    // Allow quick turns on the ground
+                    if(Velocity.X < 0 && down != null) Velocity.X = 0;
                     Velocity.X += AccelRate * dt;
                 }
-                if(Controller.MoveLeft && -MaxVel < Velocity.X)
+                else if(Controller.MoveLeft && -MaxVel < Velocity.X)
                 {
+                    // Allow quick turns on the ground
+                    if(0 < Velocity.X && down != null) Velocity.X = 0;
                     Velocity.X -= AccelRate * dt;
                 }
-                if (!(Controller.MoveLeft || Controller.MoveRight))
+                else if (!Controller.MoveLeft && !Controller.MoveRight)
                 {
-                    Velocity.X = 0;
+                    // Deaccelerate in the air to accomodate wall jumps
+                    if(down != null || Math.Abs(Velocity.X) < DeaccelRate*dt)
+                        Velocity.X = 0;
+                    else
+                        Velocity.X -= Math.Sign(Velocity.X)*DeaccelRate*dt;
                 }
-                
-                Velocity.Y += dt * Gravity;
             }
             
             // // Debug
@@ -131,15 +145,15 @@ namespace team5
             // else if(Controller.MoveDown) Velocity.Y = MaxVel;
             // else Velocity.Y = 0;
 
-            if(Controller.Jump && LongJump > 0)
+            if(Controller.Jump && 0 < LongJump)
             {
                 Velocity.Y -= AccelRate * dt;
             }
 
-            if(LongJump > 0)
+            if(0 < LongJump)
             {
                 LongJump -= dt;
-                if (Velocity.Y > 0)
+                if (0 < Velocity.Y)
                 {
                     LongJump = 0;
                 }
