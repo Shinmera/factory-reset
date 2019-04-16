34e73fe2aac8954dd3947740bde0b4b70bf9cb01
Shinmera <shinmera@tymoon.eu>
2019-04-16 11:30:57 +0200

    Convert project to use Xaml

---
+++ team5/GamePage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Windows.UI.Xaml.Controls;
+using MonoGame.Framework;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace team5
+{
+    public sealed partial class GamePage : SwapChainPanel
+    {
+        public static GamePage Self;
+        readonly Game1 Game;
+
+        public GamePage(string launchArguments)
+        {
+            this.InitializeComponent();
+            Self = this;
+            // Create the game.
+            Game = XamlGame<Game1>.Create(launchArguments, Windows.UI.Xaml.Window.Current.CoreWindow, this);
+        }
+    }
+}
