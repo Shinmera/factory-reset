1a37886c680187ba003c1406d509721f185626a6
2weirdy <lig@student.ethz.ch>
2019-04-18 14:32:26 +0200

    Added Missing file

---
+++ team5/DialogBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace team5
+{
+    class DialogBox : TextBox
+    {
+        public DialogBox(string text, string font, float sizePx, Game1 game, Level parent, Vector2 position = default(Vector2)) : base(text, font, sizePx, game, parent, "WalkieTalkieSprite", position, Chunk.Down)
+        {
+            LeftPadding = 65;
+            RightPadding = 5;
+            TopPadding = 55;
+            BottomPadding = 5;
+        }
+
+        public override void Draw()
+        {
+            base.Draw();
+
+        }
+    }
+}
