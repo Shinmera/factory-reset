4bf7795106d446887c132c54bb797978f92ca21a
Shinmera <shinmera@tymoon.eu>
2019-04-18 15:13:41 +0200

    Whoops

--- team5/Level.cs
+++ team5/Level.cs
@@ -40,9 +40,9 @@ namespace team5
         public void Pause()
         {
             Paused = !Paused;
-            //Popups.Add(new DialogBox("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
-            //    "welbut",12,Game, this,Vector2.Zero));
-            //((DialogBox)Popups.Last()).Initialize();
+            Popups.Add(new DialogBox("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
+                "welbut",12,Game, this,Vector2.Zero));
+            ((DialogBox)Popups.Last()).Initialize();
         }
 
         public Level(Game1 game, object identifier)
